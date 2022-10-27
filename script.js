3584ee40b41be10d8839f01dde42e5519eb0da0f
Neha-Patil-12 <106377584+Neha-Patil-12@users.noreply.github.com>
2022-10-27 12:52:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 for (var i=0 ;i<names.length;i++) {
 
   var firstLetter=names[i].charAt(0).toLowerCase();
-  //var firstLetter = names[i].charAt(0).toLowerCase();
+ 
  
   if (firstLetter==='j') {
     byeSpeaker.speak(names[i]);
